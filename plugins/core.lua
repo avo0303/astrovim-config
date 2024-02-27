deeee746effb50b800d178ee0b1ca7f95a75d342
Andrew Ovchinnikov <andrewdecker07@mail.ru>
2024-02-27 15:51:17 +0300

    cleanup

--- plugins/core.lua
+++ plugins/core.lua
@@ -20,6 +20,29 @@ return {
       return opts
     end,
   },
+    "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.java" },
+  {
+    "mfussenegger/nvim-jdtls",
+    opts = {
+      settings = {
+        java = {
+          configuration = {
+            runtimes = {
+              {
+                name = "JavaSE-17",
+                path = "~/.sdkman/candidates/java/17.0.9-tem/",
+              },
+              {
+                name = "JavaSE-21",
+                path = "~/.sdkman/candidates/java/21.0.2-tem/"
+              }
+            },
+          },
+        },
+      },
+    },
+  },
   -- You can disable default plugins as follows:
   -- { "max397574/better-escape.nvim", enabled = false },
   --
